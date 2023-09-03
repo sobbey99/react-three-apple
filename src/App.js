fec903cb43b31d65755c21c273182e5795016b79
sobbey99 <sobbey99@gmail.com>
2023-09-03 15:38:52 +0300

    PhoneModel animation, DesignSection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { GlobalStyle } from "./styles/GlobalStyle";
 import Quote from "./sections/Quote";
 import HeroSection from "./sections/HeroSection";
 import PhoneModel from "./sections/PhoneModel";
+import DesignSection from "./sections/DesignSection";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <GlobalStyle />
       <Quote />
       <HeroSection />
+      <DesignSection />
       {/* <PhoneModel /> */}
     </>
   );
